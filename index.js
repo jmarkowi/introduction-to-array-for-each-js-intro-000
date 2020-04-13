ed477a9d5fb072da53c63107384d268a8bfe0f9c
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-13 22:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
+
+function changeCompletely(element, index, arraay) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + `${array[index]}s!!!`;
+}
+
+var animals [ "dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+console.log(animals);
